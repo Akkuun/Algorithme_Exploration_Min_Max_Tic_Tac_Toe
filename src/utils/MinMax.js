369c98632be7ddb260c0ae51e6c6f3de29daf844
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-02-10 12:00:58 +0100

    Test alpha beta

--- src/utils/MinMax.js
+++ src/utils/MinMax.js
@@ -1,22 +1,14 @@
-import {WINNING_COMBOS} from './gameLogic';
+import { WINNING_COMBOS } from './gameLogic';
 
 class MinMax {
-
-    //get config -> return actual config
-    //isMovesLeft -> return true if there are moves remaining on the board. It returns false if there are no moves left to play (end of the game)
-    //evaluate -> evaluate the board and returns a value based on who is winning. 10 if x wins, -10 if o wins, 0 if tie
-    //minimax -> minimax function. It considers all the possible ways the game can go and returns the value of the board
-    //findBestMove -> return the best possible move for the player
 
     config;
     squareSize;
     winningCombos;
 
-    constructor(config,squareSize) {
+    constructor(config, squareSize) {
         this.config = config;
-
         this.squareSize = squareSize;
-
         this.winningCombos = new Set();
         WINNING_COMBOS(this.winningCombos, squareSize);
     }
@@ -25,10 +17,9 @@ class MinMax {
         return this.config;
     }
 
-    // This function returns true if there are moves remaining on the board. It returns false if there are no moves left to play.
     isMovesLeft() {
-        for (let i = 0; i < this.squareSize; i++){
-            for (let j = 0; j < this.squareSize; j++){
+        for (let i = 0; i < this.squareSize; i++) {
+            for (let j = 0; j < this.squareSize; j++) {
                 if (this.config[i][j] === '')
                     return true;
             }
@@ -36,10 +27,7 @@ class MinMax {
         return false;
     }
 
-    // This function evaluates the board and returns a value based on who is winning. (10 if X wins, -10 if O wins, 0 if tie)
     evaluate() {
-        console.log("evaluate");
-        // Check all combo
         for (let combo of this.winningCombos) {
             let player = this.config[combo[0][0]][combo[0][1]];
             if (!player) continue;
@@ -57,72 +45,64 @@ class MinMax {
                 }
             }
         }
-
-        // Else if none of them have won then return 0
         return 0;
     }
 
-    // This is the minimax function. It considers all the possible ways the game can go and returns the value of the board.
-    minimax(depth, isMax) {
+    minimax(depth, isMax, alpha, beta) {
         let score = this.evaluate();
 
-        // If Maximizer has won the game return his/her evaluated score
         if (score === 10)
             return score;
 
-        // If Minimizer has won the game return his/her evaluated score
         if (score === -10)
             return score;
 
-        // If there are no more moves and no winner then it is a tie
-        if (this.isMovesLeft() === false)
+        if (!this.isMovesLeft())
             return 0;
 
-        // If this maximizer's move
+        let best
         if (isMax) {
-            let best = -1000;
+            best = -10000;
 
-            // Traverse all cells
             for (let i = 0; i < this.squareSize; i++) {
                 for (let j = 0; j < this.squareSize; j++) {
-                    // Check if cell is empty
                     if (this.config[i][j] === '') {
-                        // Make the move
                         this.config[i][j] = 'x';
 
-                        // Call minimax recursively and choose the maximum value
-                        best = Math.max(best, this.minimax(depth + 1, !isMax));
+                        best = Math.max(best, this.minimax(depth + 1, !isMax, alpha, beta));
 
-                        // Undo the move
                         this.config[i][j] = '';
+
+                        alpha = Math.max(alpha, best);
+
+                        if (best >= beta)
+                            return best;
                     }
                 }
             }
-            return best;
         } else {
-            let best = 1000;
+            best = 10000;
 
-            // Traverse all cells
             for (let i = 0; i < this.squareSize; i++) {
                 for (let j = 0; j < this.squareSize; j++) {
-                    // Check if cell is empty
                     if (this.config[i][j] === '') {
-                        // Make the move
                         this.config[i][j] = 'o';
 
-                        // Call minimax recursively and choose the minimum value
-                        best = Math.min(best, this.minimax(depth + 1, !isMax));
+                        best = Math.min(best, this.minimax(depth + 1, !isMax, alpha, beta));
 
-                        // Undo the move
                         this.config[i][j] = '';
+
+                        beta = Math.min(beta, best);
+
+                        if (best <= alpha)
+                            return best;
                     }
                 }
             }
-            return best;
         }
+        return best;
     }
 
-    // This will return the best possible move for the player
     findBestMove() {
         let bestVal = -1000;
         let bestMove = {
@@ -130,21 +110,15 @@ class MinMax {
             col: -1
         };
 
-        // Traverse all cells, evaluate minimax function for all empty cells. And return the cell with optimal value.
         for (let i = 0; i < this.squareSize; i++) {
             for (let j = 0; j < this.squareSize; j++) {
-                // Check if cell is empty
                 if (this.config[i][j] === '') {
-                    // Make the move
                     this.config[i][j] = 'x';
 
-                    // Compute evaluation function for this move.
-                    let moveVal = this.minimax(0, false);
+                    let moveVal = this.minimax(0, false, -1000, 1000);
 
-                    // Undo the move
                     this.config[i][j] = '';
 
-                    // If the value of the current move is more than the best value, then update best
                     if (moveVal > bestVal) {
                         bestMove.row = i;
                         bestMove.col = j;
@@ -155,11 +129,6 @@ class MinMax {
         }
         return bestMove;
     }
-
-
-
-
-
 }
 
 export default MinMax;
